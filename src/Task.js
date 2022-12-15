fdf9b287e6c5881a4163d3c225edc281f1586ed2
amalia <amalia.19012@mhs.unesa.ac.id>
2022-12-15 15:01:45 +0700

    commit pertama

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "-" // TODO: replace this
+const studentName = "Amalia Fandaning Tyas" // TODO: replace this
+const studentId = "FE2663892" // TODO: replace this
+
+export { url, studentName, studentId }
